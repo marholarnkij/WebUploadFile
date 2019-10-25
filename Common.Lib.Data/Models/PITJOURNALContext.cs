39babf0b3d13adfdad625be77297d4ac9e4bc324
JOE T Savage <marholarnkij@hotmail.com>
2019-10-25 21:05:41 +0700

    Add Db Context

---
+++ Common.Lib.Data/Models/PITJOURNALContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+namespace Common.Lib.Data.Models
+{
+    public partial class PITJOURNALContext : DbContext
+    {
+        public IConfiguration Configuration { get; set; }
+        public PITJOURNALContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public virtual DbSet<JournalDetails> JournalDetails { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = Configuration.GetConnectionString("DataContext");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JournalDetails>(entity =>
+            {
+                entity.HasKey(e => e.Rid);
+
+                entity.Property(e => e.Amount)
+                    .HasColumnType("decimal(11, 4)")
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.CurrencyCode)
+                    .IsRequired()
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("(' ')");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("(' ')");
+
+                entity.Property(e => e.TransactionDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.TransactionId)
+                    .IsRequired()
+                    .HasColumnName("TransactionID")
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("(' ')");
+            });
+        }
+    }
+}
