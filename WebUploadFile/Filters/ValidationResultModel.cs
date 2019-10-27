8dd2fb08d62e98fe56dd2854a7687995e96c501f
JOE T Savage <marholarnkij@hotmail.com>
2019-10-27 21:47:23 +0700

    Add Filter Validate Model for support logging

---
+++ WebUploadFile/Filters/ValidationResultModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebUploadFile.Filters
+{
+    public class ValidationResultModel
+    {
+        public string Message { get; }
+        public List<ValidationError> Errors { get; }
+
+        public ValidationResultModel(ModelStateDictionary modelState)
+        {
+            Message = "Validation Failed";
+            Errors = modelState.Keys
+                .SelectMany(key => modelState[key].Errors
+                .Select(x => new ValidationError(key, x.ErrorMessage == "" ? x.Exception.Message : x.ErrorMessage)))
+                .ToList();
+        }
+        
+    }
+}
